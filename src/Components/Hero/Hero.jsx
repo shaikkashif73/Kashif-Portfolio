591c9d1f29add82da265bce121babe8ad4d09ba1
Shaik Kashif <kashifcodes10@gmail.com>
2026-08-31 15:21:53 +0530

    Creating Portfolio Navbar and Hero Section

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import profile_img from "../../assets/profile_img1.svg";
+import "./Hero.css";
+const Hero = () => {
+  return (
+    <div className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        <span>I'm Shaik Kashif,</span> frontend developer based in HYD.
+      </h1>
+      <p>
+        I am a frontend developer from Hyderabad, India with 2 years of
+        experience in web development.
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">Connect with me</div>
+        <div className="hero-resume">My resume </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
