7defc0da2ec35c622f6f67f57e9f62697a656f44
Shaik Kashif <kashifcodes10@gmail.com>
2026-09-03 03:03:52 +0530

    Created Contact Section

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import "./Contact.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import mail_icon from "../../assets/mail_icon.svg";
+import location_icon from "../../assets/location_icon.svg";
+import call_icon from "../../assets/call_icon.svg";
+const Contact = () => {
+  return (
+    <div className="contact">
+      <div className="contact-title">
+        <h1>Get in Touch</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Let's Talk</h1>
+          <p>
+            I'm currently available to take on new projects, so feel free to
+            send me a message about anything that you want me to work on. You
+            can contact anytime.
+          </p>
+          <div className="contact-details">
+            <div className="contact-detail">
+              <img src={mail_icon} alt="" />
+              <p>kashifcodes10@gmail.com</p>
+            </div>
+            <div className="contact-detail">
+              <img src={call_icon} alt="" />
+              <p>+91 70137 17836</p>
+            </div>
+            <div className="contact-detail">
+              <img src={location_icon} alt="" />
+              <p>Hyderabad, India</p>
+            </div>
+          </div>
+        </div>
+
+        <form className="contact-right">
+          <label htmlFor="">Your Name</label>
+          <input type="text" placeholder="Enter your name" name="name" />
+          <label htmlFor="">Your Email</label>
+          <input type="email" placeholder="Enter your email" name="email" />
+          <label htmlFor="">Write your message here</label>
+          <textarea
+            name="message"
+            rows="8"
+            placeholder="Enter your Message"
+          ></textarea>
+          <button className="contact-submit" type="submit">Submit now</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
